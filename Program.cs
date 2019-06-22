23a3ff1672fd644dcf4d965341a0a2080c060c7d
János Szomor <jszomor@organicawater.com>
2019-06-23 00:22:41 +0200

    First commit of PluralSight C# Fundamentals video

--- Program.cs
+++ Program.cs
@@ -2,12 +2,30 @@
 
 namespace GradeBook
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");             
-            // Console.Read();
-        }
-    }
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var numbers = new[] {12.7, 10.3, 6.11};
+			
+			var result = 0.0;
+			foreach(var number in numbers)
+			{
+				result += number;
+			}
+			System.Console.WriteLine(result);
+			
+			if (args.Length > 0)
+			{
+				Console.WriteLine($"Hello, {args[1]} ! ");				
+				/*args come from launch.json where is defined
+				launch.json control the run/debug of code				
+				 */
+			}
+			else
+			{
+				System.Console.WriteLine("Just Hello!");
+			}
+		}
+	}
 }
