c665b72549bf1d547290fe0a28c07da4149d34a8
János Szomor <jszomor@organicawater.com>
2019-06-23 23:39:29 +0200

    Book Class

--- Program.cs
+++ Program.cs
@@ -1,31 +1,18 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace GradeBook
-{
+{	
 	class Program
 	{
 		static void Main(string[] args)
-		{
-			var numbers = new[] {12.7, 10.3, 6.11};
-			
-			var result = 0.0;
-			foreach(var number in numbers)
-			{
-				result += number;
-			}
-			System.Console.WriteLine(result);
-			
-			if (args.Length > 0)
-			{
-				Console.WriteLine($"Hello, {args[1]} ! ");				
-				/*args come from launch.json where is defined
-				launch.json control the run/debug of code				
-				 */
-			}
-			else
-			{
-				System.Console.WriteLine("Just Hello!");
-			}
+		{	
+			var book = new Book("Jano's Grade Book");
+			book.AddGrade(89.1);
+			book.AddGrade(90.5);
+			book.AddGrade(77.5);
+			book.ShowStatistics();
+					
 		}
 	}
 }
